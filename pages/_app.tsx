48b822deb6fbd1bf07f95d5f77102dcc9849dc35
hguy2323546 <hguy9455@gmail.com>
2025-05-15 01:17:55 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import { PrivyProvider } from "@privy-io/react-auth";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID}
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
       config={{
         loginMethods: ["farcaster"],
-        appearance: { theme: "light" },
+        appearance: { theme: "light" }
       }}
     >
       <Component {...pageProps} />
